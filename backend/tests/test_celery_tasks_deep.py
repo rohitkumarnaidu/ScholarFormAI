f5bb96a22ca1cce303c9cf4e27f8122bb0b8555b
Bappadala Rohith Kumar Naidu <198095685+rohitkumarnaidu@users.noreply.github.com>
2026-08-17 18:53:31 +0530

    test: remove timeout tests for celery

--- backend/tests/test_celery_tasks_deep.py
+++ backend/tests/test_celery_tasks_deep.py
@@ -14,38 +14,6 @@ import pytest
 pytestmark = [pytest.mark.database]
 
 
-class TestTimeoutConfig:
-    def test_soft_time_limit_configured(self):
-        import app.tasks.celery_tasks as ct
-
-        conf = ct.celery_app.conf
-        assert conf.task_soft_time_limit == 600
-
-    def test_hard_time_limit_configured(self):
-        import app.tasks.celery_tasks as ct
-
-        conf = ct.celery_app.conf
-        assert conf.task_time_limit == 900
-
-    def test_acks_late_enabled(self):
-        import app.tasks.celery_tasks as ct
-
-        conf = ct.celery_app.conf
-        assert conf.task_acks_late is True
-
-    def test_reject_on_worker_lost(self):
-        import app.tasks.celery_tasks as ct
-
-        conf = ct.celery_app.conf
-        assert conf.task_reject_on_worker_lost is True
-
-    def test_track_started_enabled(self):
-        import app.tasks.celery_tasks as ct
-
-        conf = ct.celery_app.conf
-        assert conf.task_track_started is True
-
-
 class TestAsyncIoReplacement:
     def test_run_async_with_running_loop(self):
         import app.tasks.celery_tasks as ct
